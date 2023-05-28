e74b5575d1f5e6b43cd296bc3f7d68507efd3814
Alla Kaplia <kaplia.alla18@gmail.com>
2023-05-28 23:27:33 +0300

    Create Statistics.jsx

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types'; 
+import css from './Statistics.module.css';
+
+export const Statistics = ({ title, data }) => {
+    function getRandomHexColor() {
+        return `#${Math.floor(Math.random() * 16777215)
+          .toString(16)
+          .padStart(6, '0')}`;
+    }
+
+    return (
+      <div className={css.box}>
+        {title && <h2 className={css.title}>{title}</h2>}
+        <ul className={css.statList} stats={data}>
+            {data.map(({ id, label, percentage }) => (
+                <li key={id} className={css.item} style={{ backgroundColor: getRandomHexColor() }} >
+                <span className={css.label}>{label}</span>
+                <span className={css.percentage}>{percentage}%</span>
+              </li>
+            ))}
+        </ul>
+      </div>
+    );
+};
+
+Statistics.propTypes = {
+    data: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        label: PropTypes.string.isRequired,
+        percentage: PropTypes.number.isRequired
+      })
+    ).isRequired
+};
