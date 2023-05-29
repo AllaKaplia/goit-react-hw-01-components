1be5dcc7115bb960a37c83d0fcefeb100697e23f
Alla Kaplia <kaplia.alla18@gmail.com>
2023-05-29 15:19:30 +0300

    Create TransactionHistory.jsx

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,25 @@
+
+import css from './TransactionHistory.module.css';
+
+export const TransactionHistory = ({transactions}) => {
+    return (
+        <table className={css.transaction}>
+          <thead className={css.thead}>
+            <tr className={css.tr}>
+              <th className={css.titles}>Type</th>
+              <th className={css.titles}>Amount</th>
+              <th className={css.titles}>Currency</th>
+            </tr>
+          </thead>
+          <tbody items={transactions}>
+            {transactions.map(({ id, type, amount, currency }) => (
+            <tr key={id} className={css.data}>
+              <td className={css.td}>{type}</td>
+              <td className={css.td}>{amount}</td>
+              <td className={css.td}>{currency}</td>
+            </tr>
+            ))}
+          </tbody>
+        </table>
+    );
+}
