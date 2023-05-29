62d08f62bfb28532649b6255be235e1f85734764
Alla Kaplia <kaplia.alla18@gmail.com>
2023-05-29 15:19:27 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,9 @@ import { FriendList } from './FriendList/FriendList';
 import user from '../user.json';
 import data from '../data.json';
 import friends from '../friends.json';
+import transactions from '../transactions.json'
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+
 
 export const App = () => {
   return (
@@ -21,6 +24,9 @@ export const App = () => {
       </>
       <>
         <FriendList friends={friends} />
+      </>
+      <>
+      <TransactionHistory transactions={transactions} />;
       </>
     </>
   );
