ecebc6c49741a393fc1ede8adecedd6cb5ab4989
Alla Kaplia <kaplia.alla18@gmail.com>
2023-05-29 03:39:17 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList'; 
 import user from '../user.json';
 import data from '../data.json';
+import friends from '../friends.json';
 
 export const App = () => {
   return (
@@ -16,6 +18,9 @@ export const App = () => {
       />
       <>
         <Statistics title='Upload stats' data={data}/>
+      </>
+      <>
+        <FriendList friends={friends} />
       </>
     </>
   );
